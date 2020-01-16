68ad97fa7ec016b0ffe28f207f859d78f23aaf0b
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2020-01-15 23:41:31 -0300

    backend: add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
+
 const { atlas } = require('./credentials')
 const routes = require('./routes')
 
@@ -10,6 +12,7 @@ mongoose.connect(`mongodb+srv://${atlas.username}:${atlas.password}@cluster0-dm8
     useUnifiedTopology: true
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
